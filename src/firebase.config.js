78cc1674706254e21f2b8fd80643d3ff26323400
Dineo Mathibela <dineomathibela161@gmail.com>
2022-08-24 09:33:24 +0200

    setup

---
+++ src/firebase.config.js
@@ -0,0 +1,17 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from 'firebase/firestore';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDmQ-hEldvdFB6amVtRRpSZWEcKxyjTf5U",
+  authDomain: "market-place-38ccf.firebaseapp.com",
+  projectId: "market-place-38ccf",
+  storageBucket: "market-place-38ccf.appspot.com",
+  messagingSenderId: "113086647854",
+  appId: "1:113086647854:web:98d9b925ef35482c6299bf"
+};
+
+// Initialize Firebase
+initializeApp(firebaseConfig);
+const db = getFirestore();
+
+export default db;
