22c06a47febb8ca0ebc7c6bb73f22fc6adeedecc
Dineo Mathibela <dineomathibela161@gmail.com>
2022-08-24 15:50:58 +0200

    navigation

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route  } from 'react-router-dom';
+import NavBar from './components/NavBar';
 import Explore from './pages/explore';
 import Offers from './pages/Offers';
 import signIn from './pages/signIn';
 import signUp from './pages/signUp';
 import ForgotPassword from './pages/ForgotPassword';
+import Profile from './pages/Profile';
+
 
 function App() {
   return (
@@ -13,11 +16,12 @@ function App() {
         <Routes>
           <Route path='/' element={<Explore />}/>
           <Route path='/offers' element={<Offers />}/> 
-          <Route path='/profile' element={<signIn/>}/>
+          <Route path='/profile' element={<Profile/>}/>
           <Route path='/sign-In' element={<signIn/>}/>
           <Route path='/sign-Up' element={<signUp/>}/>
           <Route path='/forgot-password' element={<ForgotPassword/>}/>
         </Routes>
+        < NavBar />
       </Router>
     </>
   );
